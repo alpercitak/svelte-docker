ebf720c1b7b65efb7c9c8b8c01ddfd8fb2c15d52
Alper Çıtak <citak.alper@gmail.com>
2022-06-28 13:40:57 +0300

    added scenario for adapter:static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapterStatic(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
